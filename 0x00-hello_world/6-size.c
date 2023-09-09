37d49421fdb609cb80249d3a4dcce7cccac030d4
kinpluto1 <kinpluto@hotmail.com>
2023-09-09 13:08:10 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - is the start of the program
+ *
+ * Return: will return 0
+ *
+ */
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 8 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)");
+return (0);
+}
