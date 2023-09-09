a4b9d6c37b94a95f94cfdba590ce4d7f03c584d6
kinpluto1 <kinpluto@hotmail.com>
2023-09-09 08:36:48 -0700

    printf testing

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - programming
+ *
+ * Return: return value is 0
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
