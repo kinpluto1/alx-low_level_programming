6ba9bd573f1740c5285a8dc67f1fa2d581d0bc2e
kinpluto1 <kinpluto@hotmail.com>
2023-09-10 01:48:08 -0700

    first use of write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - This is where my program is written
+ *
+ * Return 1 to indicate failed execution
+ *
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 35);
+	return (1);
+}
