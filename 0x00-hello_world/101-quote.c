6270713c0bbf3eff6546a0e05887bdad5da22e14
kinpluto1 <kinpluto@hotmail.com>
2023-09-10 02:19:04 -0700

    correction for betty styling in 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 #include <unistd.h>
 
 /**
- * main - This is where my program is written
+ * main - Entry point
  *
- * Return 1 to indicate failed execution
+ * Return: 1 (Success)
  *
  */
 
 int main(void)
 {
 	write(2,
-		"\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	"\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
