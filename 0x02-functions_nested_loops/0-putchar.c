4f62ff7c3e2970f1b4f43bd89e2cf36df92b0ab4
kinpluto1 <kinpluto@hotmail.com>
2023-09-13 13:34:14 -0700

    updating _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the beginning of the program
+ *
+ * Return: always return (0)
+*/
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme for NESTED LOOPS, Lets Go
