0b388d9b3da0087a15f64e4608ab0e3ddc46441f
kinpluto1 <kinpluto@hotmail.com>
2023-09-23 20:30:18 -0700

    the times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * time_table - Prints the 9 times table, starting with zero
+ */
+
+void times_table(void)
+{
+int hori, verti, multiply;
+
+for (hori = 0; hori <= 9; hori++)
+	{
+	for (verti = 0; verti <= 9; verti++)
+	{
+	multiply = hori * verti;
+		if (verti == 0)
+		{
+		_putchar('0');
+		}
+			else
+			{
+			_putchar(',');
+			_putchar(' ');
+
+			}
+		if (multiply < 10)
+		{
+		_putchar(' ');
+		}
+			else
+			{
+			_putchar((multiply / 10) + '0');
+			}
+		
+		_putchar((multiply % 10) + '0');
+	}
+	}
+}
