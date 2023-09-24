5b81a013fed66cbfb555f0de43b982e25ea83b5c
kinpluto1 <kinpluto@hotmail.com>
2023-09-24 14:31:33 -0700

    corrected betty documentation for add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * add - Adds two integers
+ * add - Adds two integers.
  *
  * @k: this is the first integer
  *
  * @f: this is the second integer
+ *
+ * Return: the addition of k and f
  */
 
 int add(int k, int f)
