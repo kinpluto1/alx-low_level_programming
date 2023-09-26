b79d5d72940d349ce3a3fc2e26240ee891c3bdbc
kinpluto1 <kinpluto@hotmail.com>
2023-09-26 09:01:07 -0700

    updated 10 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,7 @@ int i, j, k;
 				_putchar(' ');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0')'
+				_putchar((k % 10) + '0');
 			} else if (k >= 100)
 			{
 				_putchar(',');
